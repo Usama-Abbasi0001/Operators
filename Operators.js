809cfec32d03a18717fd478b96b885bb98dc7f6d
unknown <usamakhan58461@gmail.com>
2024-06-04 22:20:45 +0500

    operators

---
+++ Operators.js
@@ -0,0 +1,65 @@
+var a = 23
+var b = 7
+var c = a + b ;
+document.write(c + " = Addtion");
+
+document.write("<br> <br>");
+
+var d = 23;
+var e = 7;
+var f = d - e;
+document.write(f + " = Subtraction");
+
+
+document.write("<br> <br>");
+
+var g = 4;
+var h = 5;
+var i = g * h;
+document.write(i + " = Multipication");
+
+document.write("<br> <br>");
+
+var j = 50;
+var k = 10;
+var l = j / k;
+document.write(l + " = Multipication");
+
+document.write("<br> <br>");
+
+var u = 50;
+var s = 10;
+u++;
+document.write(u + " = Increamint");
+
+document.write("<br> <br>");
+
+var o = 50;
+var p = 10;
+o--;
+document.write(o + " = decreamint");
+
+document.write("<br> <br>");
+
+var l = 50;
+var z = 20;
+l += z
+document.write(l + "  = Assigment Operatoers  Compound ");
+
+document.write("<br> <br>");
+
+
+//comparison operators
+var you = 30;
+var me = "30";
+document.write(you === me);
+
+document.write("<br> <br>");
+
+//logical Operators
+var age = 60;
+if (age >=18 && age <=50 ){
+    document.write("Are You Eligible")
+}else{
+    document.write("are you not eligible")
+}
